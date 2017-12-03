c83c7f824fcd042044b4cf7509781b924c23d847
Tejas Rane <tejas.m.rane@gmail.com>
2017-12-03 00:59:56 -0800

    bubble

--- src/Sorting.java
+++ src/Sorting.java
@@ -9,12 +9,21 @@ public class Sorting {
 
 	}
 	public static void bubbleSort(){
-		
+		for(int i =0; i < arr.length; i++){
+			for(int j = i+1 ; j< arr.length ; j++){
+				if(arr[i] >= arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		printArray();
 	}
 	public static void main(String[] args) {
 
 		bubbleSort();
-		System.out.println("Array After Bubble Sort");
+		
 
 	}
 
