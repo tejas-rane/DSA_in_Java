ab26f16d7151e3f15f64b14c0a6383b1aa2fd985
Tejas Rane <tejas.m.rane@gmail.com>
2017-12-03 00:55:49 -0800

    bubble

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,8 +1,20 @@
 
 public class Sorting {
+	static int arr[] = { 1, 5, 2, 6, 3, 5 };
 
+	public static void printArray() {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+	public static void bubbleSort(){
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		bubbleSort();
+		System.out.println("Array After Bubble Sort");
 
 	}
 
